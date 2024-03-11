32cd074bef5f9fa59920b3af481ae50eb1146c4c
actorpus <thealexmcbride@gmail.com>
2024-03-11 18:20:14 +0000

    Added second log for nice veiwing, fixed infiniate looping issue (I think)

--- sentry.py
+++ sentry.py
@@ -9,7 +9,8 @@ import reject_api
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger("SENTRY")
-
+_user_logger = logging.getLogger("SENTRY_USER")
+_user_logger.addHandler(logging.FileHandler("sentry.log"))
 
 
 def wait_until_next_half():
@@ -18,7 +19,7 @@ def wait_until_next_half():
     target = current + datetime.timedelta(minutes=30 - current.minute % 30)
     target = target.replace(second=0, microsecond=0)
 
-    _logger.info(f"Sleeping until {target}, {(target - current).total_seconds()} seconds left")
+    _logger.info(f"Sleeping until {target}, {(target - current).total_seconds().__floor__()} seconds left")
 
     time.sleep((target - current).total_seconds())
 
@@ -39,8 +40,11 @@ while True:
         continue
 
     _logger.info("Events found")
+    _user_logger.info(f"Events found: {', '.join([event['time'] + ' ' + event['activity'] for event in events])}")
 
     while any(event["status"] not in ["Present", "Present Late"] for event in events):
+        print(events)
+
         _logger.info("Not all events are signed into")
 
         codes = reject_api.getCodes(settings, return_events=False)
@@ -51,10 +55,14 @@ while True:
             continue
 
         _logger.info("Codes found, attempting to sign in to events")
+        _user_logger.info(f"Codes found: {', '.join(codes)}")
 
         event_checkin.try_codes(codes, session_tokens["XSRF-TOKEN"], session_tokens["prestostudent_session"])
+
+        events, token = event_checkin.get_checkin_events_token(session_tokens["prestostudent_session"])
 
         # TODO: Somehow wake the user up if any events are not signed into and have less than 15 minutes left
         #       event["time"] plus 45 minuets or implement finish time scraping
 
     _logger.info("All events are signed into")
+    _user_logger.info("All events are signed into")
